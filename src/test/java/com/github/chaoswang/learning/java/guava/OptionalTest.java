abb296c67114696d0b6b8c202e9f7e3bce3acf66
10094714 <wang.chao29@zte.com.cn>
2017-04-14 17:52:18 +0800

    guava practise

---
+++ src/test/java/com/github/chaoswang/learning/java/guava/OptionalTest.java
@@ -0,0 +1,17 @@
+package com.github.chaoswang.learning.java.guava;
+
+import org.junit.Test;
+
+import com.google.common.base.Optional;
+
+public class OptionalTest {
+
+	@Test
+	public void testOptional(){
+		Optional<Integer> possible=Optional.of(6);
+        if(possible.isPresent()){
+            System.out.println("possible isPresent:"+possible.isPresent());
+            System.out.println("possible value:"+possible.get());
+        }
+	}
+}
